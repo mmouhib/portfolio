29073c5c2f141ca71bd6ac58c62cf51672eb835d
mouhib <iwritebadcode@github.com>
2023-07-08 21:27:54 +0300

    refactor: remove small components.

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,12 +1,26 @@
-import Timeline from '../components/aboutMe/Timeline';
 import AboutMeContent from '../components/aboutMe/AboutMeContent';
 import '../styles/pages/AboutMe.scss';
+import timelineMilestones from '../static/timeline';
+import { ITimeline } from '../utils/types';
+import TimelineItem from '../components/aboutMe/TimelineItem';
 
 export default function AboutMe() {
 	return (
 		<div className="about-me" id="about-me">
 			<AboutMeContent />
-			<Timeline />
+			<div className="timeline-container">
+				{timelineMilestones.map((element: ITimeline, index: number) => {
+					return (
+						<TimelineItem
+							key={index}
+							content={element.content}
+							duration={element.duration}
+							title={element.title}
+							icon={element.icon}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,32 @@
 import Navbar from '../components/home/navbar';
-import HomeContent from '../components/home/homeContent';
 import '../styles/pages/Home.scss';
 import ResumeModal from '../components/home/resumeModal';
 import { useState } from 'react';
+import Typewriter from '../components/home/typewriter';
+import homeAvatar from '../assets/homeAvatar.png';
 
 export default function Home() {
 	const [resumeModalState, setResumeModalState] = useState<boolean>(false);
 	return (
 		<div className="home-page" data-theme="dark" id="home">
 			<Navbar setModalIsOpen={setResumeModalState} />
-			<HomeContent />
-			<ResumeModal
-				modalIsOpen={resumeModalState}
-				setModalIsOpen={setResumeModalState}
-			/>
+			<div className="home-content">
+				<div className="home-text-description">
+					<h1>
+						Hey there! i'm
+						<Typewriter />
+					</h1>
+					<h2>Junior Software developer.</h2>
+					<p className="about">
+						Hello! i'm and junior <span>mobile</span> and <span>web</span> developer looking to enhance
+						his skills and dive deeper into the software development field.
+					</p>
+				</div>
+				<div className="home-image-container">
+					<img src={homeAvatar} alt="home image" />
+				</div>
+			</div>
+			<ResumeModal modalIsOpen={resumeModalState} setModalIsOpen={setResumeModalState} />
 		</div>
 	);
 }
